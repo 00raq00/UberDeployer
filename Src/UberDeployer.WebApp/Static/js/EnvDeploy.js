320904db21339a74339329499ba37eb3538096cd
mariusz-rubin <mariusz.rubin@gmail.com>
2016-01-05 16:20:50 +0100

    Tree grid logs moved to other files

--- Src/UberDeployer.WebApp/Static/js/EnvDeploy.js
+++ Src/UberDeployer.WebApp/Static/js/EnvDeploy.js
@@ -7,11 +7,7 @@ UberDeployer.EnvDeploy = function() {
 
   var _self = this;
 
-  var _currentDiagnosticMessageGroupId = null;
-
-  var _currentDiagnosticMessageGroupName = null;
-
-  var _lastSeenMessageId = -1;
+  var _logsTreeGrid;
 
   var doDeployEnv = function() {
     var targetEnvironmentName = getSelectedTargetEnvironmentName();
@@ -51,105 +47,6 @@ UberDeployer.EnvDeploy = function() {
         });
   };
   
-  function startDiagnosticMessagesTreeLoader() {
-    loadNewDiagnosticMessagesToTree();
-
-    setTimeout(
-      function () {
-        startDiagnosticMessagesTreeLoader();
-      },
-      g_DiagnosticMessagesLoaderInterval);
-  }
-
-  function loadNewDiagnosticMessagesToTree() {
-    $.getJSON(
-      g_AppPrefix + 'Api/GetDiagnosticMessages?lastSeenMaxMessageId=' + _lastSeenMessageId,
-      function (data) {
-        $.each(data.messages, function (i, val) {
-          if (val.MessageId > _lastSeenMessageId) {
-            _lastSeenMessageId = val.MessageId;
-            logMessageTree(val.Message, val.Type, val.GroupName, val.MessageId);
-          }
-        });
-      });
-  }
-
-  function logMessageTree(message, type, groupName, messageId) {
-    var $treeLogs = $('#tree-logs');
-    var $logRow = $('<tr></tr>');
-    $logRow.addClass("treegrid-" + messageId);
-    
-    var $logCell = $("<td></td>");
-    $logCell.text(">> " + message);
-
-    $logCell.addClass('log-msg');
-
-    setLogType($logRow, type);
-
-    if (!groupName) {
-      // add regular log
-      _currentDiagnosticMessageGroupName = null;
-      _currentDiagnosticMessageGroupId = null;
-    }
-    else if (_currentDiagnosticMessageGroupName == groupName) {
-      // add to existing group
-      $logRow.addClass("treegrid-parent-" + _currentDiagnosticMessageGroupId);
-      
-      if (type.toLowerCase() == "error") {
-        var $groupRow = $('.' + 'treegrid-' + _currentDiagnosticMessageGroupId);
-
-        var logClassPattern = /log-msg-*/;
-
-        $groupRow[0].className.split(/\s+/)
-          .filter(function(value) {
-            return logClassPattern.test(value);
-          }).forEach(function(logClass) {
-            $groupRow.removeClass(logClass);
-          });
-        
-        $groupRow.addClass("log-msg-error");
-      }
-    }
-    else {
-      // create new group
-      _currentDiagnosticMessageGroupName = groupName;
-      _currentDiagnosticMessageGroupId = messageId;
-
-      $logRow.addClass("active");
-    }
-
-    $logRow.append($logCell);
-    $treeLogs.append($logRow);
-    $treeLogs.treegrid({
-       "initialState":"collapsed"
-    });
-
-    //$treeLogs.scrollTop($treeLogs[0].scrollHeight - $treeLogs.height());
-  }
-  
-  function setLogType(logRow, type) {
-    var typeLower = type.toLowerCase();
-
-    if (typeLower === 'trace') {
-      logRow.addClass('log-msg-trace');
-    }
-    else if (typeLower == 'info') {
-      logRow.addClass('log-msg-info');
-    }
-    else if (typeLower == 'warn') {
-      logRow.addClass('log-msg-warn');
-    }
-    else if (typeLower == 'error') {
-      logRow.addClass('log-msg-error');
-    }
-  }
-
-  function clearLogs() {
-    var $txtLogs = $('#txt-logs');
-
-    $txtLogs.find('*').remove();
-  }
-
   var setupSignalR = function() {
     var deploymentHub = $.connection.deploymentHub;
 
@@ -182,7 +79,7 @@ UberDeployer.EnvDeploy = function() {
     };
 
     return ConfirmRestoreDialog;
-  })();
+  }());
 
   return {
     initializeEnvDeploymentPage : function(initData) {
@@ -218,8 +115,9 @@ UberDeployer.EnvDeploy = function() {
         loadProjectsForCurrentEnvironmentDeploy();
       });
 
-      $('#tree-logs').treegrid();
-      startDiagnosticMessagesTreeLoader();
+      _logsTreeGrid = UberDeployer.createLogsTreeGrid(g_AppPrefix, '#tree-logs', g_DiagnosticMessagesLoaderInterval);
+      
+      _logsTreeGrid.startTreeGridLogsLoader();
     }
   };
 }();
---
+++ Src/UberDeployer.WebApp/Static/js/treeGridLogs.js
@@ -0,0 +1,109 @@
+﻿var UberDeployer = UberDeployer || {};
+
+UberDeployer.createLogsTreeGrid = function (appPrefix, treeLogsTableSelector, logsLoaderInterval) {
+
+  var _treeLogsTableSelector = treeLogsTableSelector;
+  var _appPrefix = appPrefix;
+  var _logsLoaderInterval = logsLoaderInterval;
+  var _currentDiagnosticMessageGroupId = null;
+  var _currentDiagnosticMessageGroupName = null;
+  var _lastSeenMessageId = -1;
+
+  function loadNewDiagnosticMessagesToTree() {
+    $.getJSON(
+      _appPrefix + 'Api/GetDiagnosticMessages?lastSeenMaxMessageId=' + _lastSeenMessageId,
+      function(data) {
+        $.each(data.messages, function(i, val) {
+          if (val.MessageId > _lastSeenMessageId) {
+            _lastSeenMessageId = val.MessageId;
+            logMessageTree(val.Message, val.Type, val.GroupName, val.MessageId);
+          }
+        });
+      });
+  }
+
+  function logMessageTree(message, type, groupName, messageId) {
+    var $treeLogs = $(_treeLogsTableSelector);
+    var $logRow = $('<tr></tr>');
+    $logRow.addClass("treegrid-" + messageId);
+
+    var $logCell = $("<td></td>");
+    $logCell.text(">> " + message);
+
+    $logCell.addClass('log-msg');
+
+    setLogType($logRow, type);
+
+    if (!groupName) {
+      // add regular log
+      _currentDiagnosticMessageGroupName = null;
+      _currentDiagnosticMessageGroupId = null;
+    } else if (_currentDiagnosticMessageGroupName == groupName) {
+      // add to existing group
+      $logRow.addClass("treegrid-parent-" + _currentDiagnosticMessageGroupId);
+
+      if (type.toLowerCase() == "error") {
+        var $groupRow = $('.' + 'treegrid-' + _currentDiagnosticMessageGroupId);
+
+        var logClassPattern = /log-msg-*/;
+
+        $groupRow[0].className.split(/\s+/)
+          .filter(function(value) {
+            return logClassPattern.test(value);
+          }).forEach(function(logClass) {
+            $groupRow.removeClass(logClass);
+          });
+
+        $groupRow.addClass("log-msg-error");
+      }
+    } else {
+      // create new group
+      _currentDiagnosticMessageGroupName = groupName;
+      _currentDiagnosticMessageGroupId = messageId;
+
+      $logRow.addClass("active");
+    }
+
+    $logRow.append($logCell);
+    $treeLogs.append($logRow);
+    $treeLogs.treegrid({
+      "initialState": "collapsed"
+    });
+
+    //$treeLogs.scrollTop($treeLogs[0].scrollHeight - $treeLogs.height());
+  }
+
+  function setLogType(logRow, type) {
+    var typeLower = type.toLowerCase();
+
+    if (typeLower === 'trace') {
+      logRow.addClass('log-msg-trace');
+    } else if (typeLower == 'info') {
+      logRow.addClass('log-msg-info');
+    } else if (typeLower == 'warn') {
+      logRow.addClass('log-msg-warn');
+    } else if (typeLower == 'error') {
+      logRow.addClass('log-msg-error');
+    }
+  }
+
+  function startLogsLoader() {
+    loadNewDiagnosticMessagesToTree();
+
+    setTimeout(
+      startLogsLoader,
+      _logsLoaderInterval);
+  }
+
+  return {
+    startTreeGridLogsLoader: function () {
+      $(_treeLogsTableSelector).treegrid();
+      
+      startLogsLoader();
+    },
+
+    clearLogs: function () {
+      $(_treeLogsTableSelector).find('*').remove();
+    }
+  };
+};
